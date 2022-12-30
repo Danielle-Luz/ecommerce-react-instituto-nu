0d73a751a09dfe627b7d30f9fdcf3d41aa79ff62
Axionauta <daniln2209@gmail.com>
2022-12-29 22:42:06 -0300

    feat: estiliza header

--- src/components/header/styles.tsx
+++ src/components/header/styles.tsx
@@ -1,5 +1,13 @@
 import styled from "styled-components";
+import { theme } from "../../styles/theme";
 
 export const HeaderStyled = styled.header`
-
+  background-color: ${({theme : iTheme}) => theme.colors.grey[4]};
+  padding: 28px 0;
+  
+  div {
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+  }
 `
