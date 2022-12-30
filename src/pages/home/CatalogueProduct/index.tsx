a01158fb49a09c9aeb202f983cac4c6aa32e434c
Axionauta <daniln2209@gmail.com>
2022-12-30 10:27:15 -0300

    feat: adiciona estrutura do produto

--- src/pages/home/CatalogueProduct/index.tsx
+++ src/pages/home/CatalogueProduct/index.tsx
@@ -10,7 +10,12 @@ interface iCatalogueProductProps {
 export function CatalogueProduct ({img, title, price, category} : iCatalogueProductProps) {
   return (
     <CatalogueProductStyled>
-
+      <img src={img} alt={title} />
+      <article>
+        <h2>{title}</h2>
+        <span className="catalogue-product__price">{price}</span>
+        <span className="catalogue-product__category">{category}</span>
+      </article>
     </CatalogueProductStyled>
   );
 }
