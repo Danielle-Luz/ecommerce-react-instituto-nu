9c91588b8d60a0ff88be22e0dc8c637b2bb7279c
Axionauta <daniln2209@gmail.com>
2022-12-30 11:09:34 -0300

    feat: adiciona responsividade na grid

--- src/pages/home/Catalogue/styles.ts
+++ src/pages/home/Catalogue/styles.ts
@@ -3,7 +3,15 @@ import styled from "styled-components";
 export const CatalogueStyled = styled.ul`
   display: grid;
   gap: 20px;
-  grid-template-columns: repeat(3, minmax(100px, 300px));
   list-style-type: none;
   padding: 20px;
+  grid-template-columns: repeat(1, minmax(100px, 300px));
+
+  @media (min-width: 480px) {
+    grid-template-columns: repeat(2, minmax(100px, 300px));
+  }
+
+  @media (min-width: 680px) {
+    grid-template-columns: repeat(3, minmax(100px, 300px));
+  }
 `
