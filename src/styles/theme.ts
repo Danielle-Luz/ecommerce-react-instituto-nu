a67215122fba34a44d2b730ea3c29656f27ec841
Axionauta <daniln2209@gmail.com>
2022-12-29 22:13:16 -0300

    feat: cria theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export interface iTheme {
+  colors: {
+    grey : string[],
+    primary : string[]
+  },
+  radius : string
+}
+
+export const theme : iTheme = {
+  colors: {
+    grey: ["333333", "828282", "BDBDBD", "E0E0E0", "F5F5F5"],
+    primary: ["27AE60", "21A759"]
+  },
+  radius: "8px"
+}
