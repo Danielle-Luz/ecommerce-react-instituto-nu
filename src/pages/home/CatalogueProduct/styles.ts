7f23eedb5956c161bc0bca715431b61b6e2d1e57
Axionauta <daniln2209@gmail.com>
2022-12-30 10:52:35 -0300

    feat: adiciona estilização no produto

--- src/pages/home/CatalogueProduct/styles.ts
+++ src/pages/home/CatalogueProduct/styles.ts
@@ -1,5 +1,31 @@
 import styled from "styled-components";
+import { theme } from "../../../styles/theme";
 
 export const CatalogueProductStyled = styled.li`
+  border: 1px solid ${({theme : iTheme}) => theme.colors.grey[2]};
 
+  h2 {
+    color: ${({theme : iTheme}) => theme.colors.grey[0]};
+    font-size: 18px;
+    font-weight: 700;
+  }
+
+  .catalogue-product__price {
+    color: ${({theme : iTheme}) => theme.colors.primary[0]};
+    font-size: 16px;
+    font-weight: 600;
+  }
+  
+  .catalogue-product__category {
+    color: ${({theme : iTheme}) => theme.colors.grey[1]};
+    font-size: 14px;
+    font-weight: 400;
+  }
+
+  article {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    padding: 20px;
+  }
 `
