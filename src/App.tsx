2fec716e97025009dfb2bd107ff1ea6fcaa4fb13
Axionauta <daniln2209@gmail.com>
2022-12-29 22:22:45 -0300

    feat: adiciona global styles e theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./styles/GlobalStyles";
+import { theme } from "./styles/theme";
+
 export function App() {
-  return <></>;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+      </ThemeProvider>
+    </>
+  );
 }
