3e608f8e6a43ddde97ba958e39c85cbed2ac0ab8
Axionauta <daniln2209@gmail.com>
2022-12-30 15:17:05 -0300

    feat: adiciona box shadow no header

--- src/components/header/styles.tsx
+++ src/components/header/styles.tsx
@@ -3,6 +3,7 @@ import { theme } from "../../styles/theme";
 
 export const HeaderStyled = styled.header`
   background-color: ${({theme : iTheme}) => theme.colors.grey[4]};
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.25);
   padding: 28px 0;
   position: sticky;
   top: 0;
